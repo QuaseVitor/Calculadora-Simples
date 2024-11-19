70e924db6c8ed561fe3de790d26624966cdba529
QuaseVitor <Jvitorcont@gmail.com>
2024-11-18 22:34:30 -0300

    Envio de arquivos

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Calculadora Simples em Python
+
+Este projeto é uma calculadora simples desenvolvida em Python, criada para fins de estudo e aprendizado. O objetivo é entender a implementação de operações básicas de cálculo como adição, subtração, multiplicação e divisão.
+
+## Funcionalidades
+
+A calculadora oferece as seguintes funcionalidades:
+
+- **Soma**: Adiciona dois números.
+- **Subtração**: Subtrai o segundo número do primeiro.
+- **Multiplicação**: Multiplica dois números.
+- **Divisão**: Divide o primeiro número pelo segundo.
+
+## Tecnologias Utilizadas
+
+- **Python 3.x**: Linguagem de programação principal utilizada para a construção do projeto.
+
+## Como Rodar
+
+1. Clone este repositório ou baixe os arquivos para o seu computador.
+2. Certifique-se de ter o Python 3.x instalado. Você pode verificar se o Python está instalado executando o comando:
+
+   ```bash
+   python --version
+
+3. Execute o comando main.py e veja funcionando
---
+++ main.py
@@ -0,0 +1,59 @@
+#Calculadora com while
+
+while True:
+    numero_1 = input('insira o numero ')
+    numero_2 = input("insira o outro numero ")
+    
+    operador = input("o que deseja fazer? (+-/*) ")
+    
+    numeros_validos = None
+    
+    
+    ##Validando numeros
+    
+    try:
+        numero_1 = float(numero_1)
+        numero_2 = float(numero_2)
+        
+        numeros_validos = True
+        
+        print("tudo ok")
+    except:
+        pass   
+    
+    if numeros_validos is None:
+        print("numeros invalidos")
+        continue
+       
+    ##Validando operador
+    
+    
+    operadores_permitidos = '+-/*' 
+    if len(operador) > 1:
+        print('apenas um operador')
+        continue
+    
+    if operador not in operadores_permitidos:
+        print("Não esta nos operadores")
+        continue
+       
+       
+    if operador == "+":
+        print(numero_1+numero_2)
+    elif operador == "-":
+        print(numero_1-numero_2)
+    elif operador == "/":
+        print(numero_1/numero_2)
+    elif operador == "*":
+        print(numero_1*numero_2)
+    
+        
+        
+        
+        
+    sair = input("quer sair?S/N ").upper().startswith('S')
+    
+    if sair:
+        print("Saiu") 
+        break
+    
